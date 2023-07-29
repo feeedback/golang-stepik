e518d0fa0461437eec31d8a21a90deaa4221fb8e
Alexander Rzhanitsyn <risdeep@gmail.com>
2023-07-30 01:45:34 +0400

    add lesson_1_6\1, lesson_1_7\1

---
+++ lesson_1_6/1/main.go
@@ -0,0 +1,10 @@
+// https://stepik.org/lesson/228262/step/3
+package main
+
+import "fmt"
+
+func main() {
+	var a2 int = 10
+	a2 = a2 * 10
+	fmt.Println(a2)
+}
---
+++ lesson_1_7/1/main.go
@@ -0,0 +1,11 @@
+// https://stepik.org/lesson/228264/step/2
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 8
+	const b int = 10
+	a = a + b
+	fmt.Println(a)
+}
