549defaa2b467a004ec7ea442dc463d4acee5098
Alexander Rzhanitsyn <risdeep@gmail.com>
2023-08-12 17:33:25 +0400

    add lesson_2_7\5

---
+++ lesson_2_7/5/main.go
@@ -0,0 +1,15 @@
+package main // https://stepik.org/lesson/229321/step/7
+
+/*
+func T() float64 {
+	return 6 / W()
+}
+
+func W() float64 {
+	return math.Sqrt(k / M())
+}
+
+func M() float64 {
+	return p * v
+}
+*/
